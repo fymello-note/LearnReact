2ba52b39cfefdbefe32fe2b413c9c1a78070ba9d
Matteo Massimello <matteo.massimello@gmail.com>
2026-01-19 14:02:51 +0100

    feat: add of the properties

--- src/App.js
+++ src/App.js
@@ -1,59 +1,23 @@
-// curly braces in react component
+import { getImage } from "./utils"
 
-export function Avatar(){
-	const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
-	const description = 'new image';
+// introducing properties: way to send parameters to component
+function Avatar({person, size}){
 	return (
-		<img
+		<img 
 			className="avatar"
-			src={avatar}
-			alt={description}
+			src={getImage(person)}
+			alt="fulvios"
+			width={size}
+			height={size}
 		/>
 	)
 }
 
-export function TodoList(){
-	const name = "fulvios";
-
+export default function Profile(){
 	return (
-		<h1>{name}'s To Do List</h1>
-	);
-}
-
-
-// double curlies: CSS and other objects in JSX
-
-// export default function TodoList3(){
-// 	let b = 'black';
-// 	let s = {backgroundColor: b, color: 'pink'};
-// 	return (
-// 		<ul style={s}>
-// 			<li>point1</li>
-// 			<li>point2</li>
-// 			<li>point3</li>
-// 		</ul>
-// 	)
-// }
-
-const person = {
-	name: "person1",
-	theme: {
-		backgroundColor: "black",
-		color: "pink"
-	}
-}
-
-export default function TodoList3(){
-	let b = 'black';
-	let s = {backgroundColor: b, color: 'pink'};
-	return (
-		<div style={person.theme}>
-			<h1>{person.name}</h1>
-			<img
-				className="avatar"
-				src='https://i.imgur.com/7vQD0fPs.jpg'
-				alt='new image'
-			/>
-		</div>
+		<Avatar 
+			person={{imageId: "MK3eW3Am"}}
+			size={100}
+		/>
 	)
 }
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export function getImage(person, size='s') {
+    return 'https://i.imgur.com/' + person.imageId + '.jpg'; 
+}
